64bb6a158565cdf780fc1cce5394877083480ac7
MauLight <maulisseluz@gmail.com>
2023-03-19 04:50:15 -0300

    basic ai added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import { Patterns } from './components/patterns';
 
 function App() {
 
+
+  //Create state for board, current player, the result of the game, declaring a winner.
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState("O");
+  const [player, setPlayer] = useState("X");
   const [result, setResult] = useState({ winner: "none", state: "none" });
   const [winner, setWinner] = useState(false);
+  const [title, setTitle] = useState("TIC-TAC-TOE");
+  const [gameStart, setGameStart] = useState(false);
+  const [aiBoard, setAiBoard] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => { //check if there's a winner or a tie after every render, also changes current player.
+    //setGameStart(false);
     checkWinner();
     checkTie();
 
+    /*
     if (player === "X") {
-      setPlayer("O")
+      setPlayer("O");
     }
     else {
-      setPlayer("X")
-    };
+      setPlayer("X");
+    }; */
 
   }, [board]);
 
@@ -28,16 +35,23 @@ function App() {
       alert(`Game Over! Winner is ${result.winner}`);
       setResult({ winner: "none", state: "none" });
     }
-  })
+  });
 
-  const chooseBlock = (block) => {
-    setBoard(board.map((val, index) => {
+  const chooseBlock = (block) => { //block = index of block pressed (position between 0 and 8).
+    setGameStart(true);
+    const playerBoard = board.map((val, index) => { //val = X or O chosen.
       if (index === block && val === "") {
         return player;
       }
       return val;
-    })
-    );
+    });
+
+    console.log(playerBoard);
+    aiChoose(playerBoard);
+
+
+    //aiChoose();
+
   }
 
   const checkWinner = () => {
@@ -64,7 +78,7 @@ function App() {
 
   const checkTie = () => {
     //let filled = false;
-    const checkTie = board.every((block) => block !== "" && winner == false);
+    const checkTie = board.every((block) => block !== "" && winner === false);
 
     if (checkTie) {
       console.log(board)
@@ -76,9 +90,57 @@ function App() {
   }
 
 
+  const aiChoose = (arr) => {
+    const emptySpots = [];
+    let newArr = arr.map(elem => elem);
+
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] === "") {
+        emptySpots.push(i);
+      }
+    };
+
+    console.log(emptySpots);
+
+    let randomNumber = Math.floor(Math.random() * emptySpots.length);
+    console.log(randomNumber);
+    console.log(emptySpots[randomNumber]);
+
+    newArr[emptySpots[randomNumber]] = "O";
+    console.log(newArr[emptySpots[randomNumber]]);
+    setBoard(newArr);
+    setGameStart(false);
+
+    console.log(newArr);
+
+
+    /*
+        let randomNumber = Math.floor(Math.random() * 8) + 1;
+        if (board[randomNumber] === "" && gameStart && player === "O") {
+          const filterFunction = (value, index) => {
+            if (value === "" && index === randomNumber) {
+              return player;
+            }
+            else {
+              return value;
+            }
+          }
+          const aiBoard = board.map(filterFunction);
+          console.log(aiBoard);
+          setBoard(aiBoard);
+          setGameStart(false); 
+        } */
+  }
+
+
+
   return (
     <div className="App">
+      <div className='title'>
+        <h1>{title}</h1>
+      </div>
       <div className='board'>
+
         <div className='row'>
           <Block val={board[0]} chooseBlock={() => { chooseBlock(0) }} />
           <Block val={board[1]} chooseBlock={() => { chooseBlock(1) }} />
