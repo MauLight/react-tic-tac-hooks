e19c2e2b40349f6cf0583c172dc0b1450006daf8
MauLight <maulisseluz@gmail.com>
2023-03-19 01:07:56 -0300

    game finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Block } from './components/block';
+import { Patterns } from './components/patterns';
 
 function App() {
 
   const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
-  const [player, setPlayer] = useState("X");
+  const [player, setPlayer] = useState("O");
+  const [result, setResult] = useState({ winner: "none", state: "none" });
+
+  useEffect(() => {
+    checkWinner();
+    checkTie();
+
+    if (player === "X") {
+      setPlayer("O")
+    }
+    else {
+      setPlayer("X")
+    };
+
+  }, [board]);
+
+  useEffect(() => {
+    if (result.state !== 'none') {
+      alert(`Game Over! Winner is ${result.winner}`);
+    }
+  })
 
   const chooseBlock = (block) => {
     setBoard(board.map((val, index) => {
@@ -15,14 +36,41 @@ function App() {
       return val;
     })
     );
+  }
 
-    if (player === "X") {
-      setPlayer("O")
+  const checkWinner = () => {
+    Patterns.forEach((pattern) => {
+      const possibleWinner = board[pattern[0]];
+      if (possibleWinner === "") return;
+      let foundWinningPattern = true;
+      pattern.forEach((val) => {
+        if (board[val] !== possibleWinner) {
+          foundWinningPattern = false;
+        }
+      });
+
+      if (foundWinningPattern) {
+        setResult({ winner: player, state: "won" })
+      }
+
+    })
+  }
+
+  const checkTie = () => {
+    let filled = true;
+    board.forEach((block) => {
+      if(block === "") {
+        filled = false;
+      }
+    })
+
+    if (filled) {
+      console.log(board);
+      setResult({
+        winner: "No winners",
+        state: "Tie"
+      })
     }
-    else {
-      setPlayer("X")
-    };
-
   }
 
 
@@ -30,19 +78,19 @@ function App() {
     <div className="App">
       <div className='board'>
         <div className='row'>
-          <Block val={board[0]} chooseBlock={() => {chooseBlock(0)}} />
-          <Block val={board[1]} chooseBlock={() => {chooseBlock(1)}} />
-          <Block val={board[2]} chooseBlock={() => {chooseBlock(2)}} />
+          <Block val={board[0]} chooseBlock={() => { chooseBlock(0) }} />
+          <Block val={board[1]} chooseBlock={() => { chooseBlock(1) }} />
+          <Block val={board[2]} chooseBlock={() => { chooseBlock(2) }} />
         </div>
         <div className='row'>
-          <Block val={board[3]} chooseBlock={() => {chooseBlock(3)}} />
-          <Block val={board[4]} chooseBlock={() => {chooseBlock(4)}} />
-          <Block val={board[5]} chooseBlock={() => {chooseBlock(5)}} />
+          <Block val={board[3]} chooseBlock={() => { chooseBlock(3) }} />
+          <Block val={board[4]} chooseBlock={() => { chooseBlock(4) }} />
+          <Block val={board[5]} chooseBlock={() => { chooseBlock(5) }} />
         </div>
         <div className='row'>
-          <Block val={board[6]} chooseBlock={() => {chooseBlock(6)}} />
-          <Block val={board[7]} chooseBlock={() => {chooseBlock(7)}} />
-          <Block val={board[8]} chooseBlock={() => {chooseBlock(8)}} />
+          <Block val={board[6]} chooseBlock={() => { chooseBlock(6) }} />
+          <Block val={board[7]} chooseBlock={() => { chooseBlock(7) }} />
+          <Block val={board[8]} chooseBlock={() => { chooseBlock(8) }} />
         </div>
       </div>
     </div>
